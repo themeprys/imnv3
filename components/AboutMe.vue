f62ac570e450e784b94c30ac3beb3f0eca3c2b6c
Priyo S <themeprys@gmail.com>
2022-05-22 12:09:45 +0700

    ubah about me

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -3,15 +3,17 @@
     <div class="columns">
         <div class="column">
             <h2 class="is-size-1 has-text-weight-bold has-text-centered">Tentang Saya</h2>
-            <p class="is-size-6 has-text-centered subtitle pb-6">Hello, I am Iman Fattah, and I am a musician, sound artist, and a writer from Indonesia<br>currently based in Los Angeles, CA.</p>
+            <p class="is-size-6 has-text-centered subtitle pb-6">Profil Singkat Iman Fattah</p>
         </div>
     </div>
     <div class="columns">
         <div class="column imn_about_pic">
                 <img src="~/static/img/iman_fattah_about.jpg" />
         </div>
-        <div class="column is-9 imn_about_content">
-<p>Hello world, my name is Iman Fattah. I am a musician, audio technologist, and machine learning engineer from Indonesia, currently based in Los Angeles, CA.<br /><br />I have worked on various projects such as music production, sound design, sound for theater, film scoring, gallery exhibition, and fashion runway, to name a few. I accept projects that are both meaningful for me or aesthetically challenging: from non-profit independent experiment shows to big commercial projects for advertising agencies. Yes, I have bills to pay, but I never let it limit my creative pursuit.<br /><br />In addition to music and sound design, growing up in the &lsquo;90s means that I grew up alongside the video game and computer industry. Being a hardcore gamer myself, I am knowledgeable in multiple genres of gaming that shape today&rsquo;s video game landscape. The &rsquo;90 have also shaped my life as I witnessed how video game consoles developed from analog to digital format&mdash;directing my interest in both analog and digital music production. Being a musician, producer, writer, and gamer equips me with unique audio and visual sensibilities&mdash;utilizing technology as a tool for art-making.<br /><br />Now, I am fortunate to shift career and contribute to the world&rsquo;s top tech companies based in Silicon Valley, and working at the intersection of art and technology, all thanks to my experience with music and sound that brings me to where I am today.</p>
+        <div class="column is-10 imn_about_content">
+<p>Halo, nama saya Iman Fattah. Saya seorang musisi, teknolog audio, dan <em>machine learning engineer</em> asal jakarta, Indonesia yang saat ini berdomisili di Los Angeles, California.</p>
+<p>Saya telah mengerjakan berbagai proyek musik dan audio yang diantaranya adalah produksi musik, desain bunyi (<em>sound design</em>), musik untuk teater, film, pameran, serta musik untuk peragaan busana. Pengalaman saya dalam berkarya sebagai musisi, produser, dan melalui hobi <em>video game</em> membekali saya dengan kepekaan audio dan visual yang unik&mdash;yaitu memanfaatkan teknologi berkelindan dengan seni-budaya sebagai sarana dalam berkarya.</p>
+<p>Saat ini, saya bersyukur memiliki privilese untuk mengubah karir ke dunia teknologi dan berkontribusi untuk perusahaan teknologi terbesar di dunia yang berbasis di Silicon Valley. Semua berkat pengalaman dalam musik dan audio yang membawa saya ke posisi saat ini.</p>
         </div>        
     </div>
 
